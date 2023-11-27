0b4efc7de098ee4a301cc34b62eef625309222c9
Ibekwe Testimony <ibekwetestimony3@gmail.com>
2023-11-26 22:39:05 -0800

    test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,12 @@
--- the test
--- Create a new database or use if it exists: hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Create or introduce the user hbnb_dev and set the password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges to hbnb_dev on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege to hbnb_dev on the database performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- this script prepares a MySQL server for the project
+-- create project testing database with the name : hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- granting the SELECT privilege for the user hbnb_test on the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- granting all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
